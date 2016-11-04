0f7fe3bd1cabd474509cc7dc9b147b651de13587
Alex Sullivan <xander.sullivan@gmail.com>
2016-11-04 13:36:07 +0000

    added add_picture_to_users like the carrierwave directions say

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  mount_uploader :photo, PictureUploader
+  mount_uploader :picture, PictureUploader
 
   has_many :places
   has_many :comments   
